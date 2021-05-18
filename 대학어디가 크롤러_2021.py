6d33957e95ad9536d8e6708a12554c7d940a350d
pealz01 <61739822+pealz01@users.noreply.github.com>
2021-05-18 10:30:33 +0900

    Update and rename 대학어디가 크롤러_2019.py to 대학어디가 크롤러_2021.py

--- 대학어디가 크롤러_2021.py
+++ 대학어디가 크롤러_2021.py
@@ -111,26 +111,25 @@ def get_next_page():
     else:
     #마지막 페이지가 아닐 경우 페이지 넘기기
         driver.find_element_by_css_selector("[class='next']").click()
-        time.sleep(1) #ajax 응답이 늦은 관계로 페이지 넘어간 이후 1초 쉬기
+        time.sleep(1) #ajax 응답이 늦은 관계로 페이지 넘어간 이후 1초 쉬
         return True
     
 if __name__=='__main__':
     start_time=time.time()
-    dir = "Chrome webdriver 경로 설정"
-    driver=webdriver.Chrome(dir, chrome_options=options)
-    year = '2019' #입시결과 수집 대상 년도
+    driver=webdriver.Chrome('C:/Users/user/Desktop/chromedriver', chrome_options=options)
     driver.get('https://www.adiga.kr/')
     
     #그냥 손으로 로그인하기 / 키보드 보안 프로그램 때문에 send_keys 메소드 안먹음 자동 로그인 포기
-    input("----------------------------------------직접 로그인 한 이후 아무 키나 입력----------------------------------------")
+    
+    input(str("▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒직접 로그인 한 이후 아무 키나 입력▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒"))
     
     #검색창으로 이동
     driver.get('https://www.adiga.kr/PageLinkAll.do?link=/kcue/ast/eip/eis/inf/sjinf/eipSjinfGnrl.do&p_menu_id=PG-EIP-05101')
 
-    #라디오 버튼 내 년도 설정
+    #라디오 버튼 내 년도 설정 2019년으로 설정
     radio=driver.find_elements_by_xpath('//*[@id="sch_year"]')[1]
     select = Select(radio)
-    select.select_by_value(year)
+    select.select_by_value('2021')
     
     driver.find_element_by_class_name('search_box_btn').click()   
 
@@ -141,7 +140,7 @@ if __name__=='__main__':
     page = 1
     while flg:
         
-        for i in [1,2,3]: # 모집단위 별로 페이지 이동 시 ajax call이 경우에 따라 늦게 받아오는 경우가 있어  총 3회 까지 시도
+        for i in [1,2,3]:
             try:
                 tmp, tmp2 = get_page_info()
                 if len(tmp) > 0:
@@ -151,7 +150,7 @@ if __name__=='__main__':
                     err+=tmp2
                     
                 flg = get_next_page()
-                print('----------------------------------------{}페이지 완료----------------------------------------'.format(page))
+                print('▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒{}페이지 완료▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒'.format(page))
                 page+=1
                 element = WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.CSS_SELECTOR, "[class='btn_result']")))
                 break
@@ -164,14 +163,16 @@ if __name__=='__main__':
                     page+=1
                 else:
                     pass_y_n = ''
-                    print('----------------------------------------{0}페이지 {1}차 시도{2}----------------------------------------'.format(page, i, pass_y_n))
+                    print('▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒{0}페이지 {1}차 시도{2}▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒'.format(page, i, pass_y_n))
                     time.sleep(1)
                 pass
             
 
         
     
-    col_nm=['대학명','모집단위','모집시기','전형유형','전형명','모집인원','모집인원','경쟁률','기준','학생부_환산점','등급','수능_환산점','수능_백분위','수능_등급']
+    col_nm=['대학명','모집단위','모집시기','전형유형','전형명','모집인원','전년모집인원','전년경쟁률',
+            '학생부_환산70%컷','학생부_등급50%컷','학생부_등급70%컷',
+            '수능_환산70%컷','수능_등급70%컷','수능_국백70%컷','수능_수백70%컷','수능_탐백70%컷','수능_평백70%컷','추가안내사항']
     
     df = pd.DataFrame(data=result,columns=col_nm)
     df = df.sort_values(by=['대학명','모집시기','전형유형','모집단위'])
@@ -183,11 +184,10 @@ if __name__=='__main__':
     df3 = pd.DataFrame(data=err_page,columns=['pass페이지']) if len(err_page) >0  else pd.DataFrame()
     
     #저장할 위치 확인
-    save_dir = '저장 경로 설정'
-    writer = pd.ExcelWriter('{}/2019_대학알리미_입결.xlsx'.format(save_dir))
+    writer = pd.ExcelWriter('C:/Users/user/Desktop/2021_대학알리미_입결.xlsx')
     df.to_excel(writer, sheet_name="입시결과",index=False)
     df2.to_excel(writer, sheet_name="오류모집단위",index=False)
     df3.to_excel(writer, sheet_name="오류페이지",index=False)
     writer.save()
     
-    print('----------------------------{0} mins - {1} to {2}----------------------------'.format((time.time()-start_time)/60),start_time,time.time())
+    print('▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒{0} mins - {1} to {2}▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒'.format((time.time()-start_time)/60),start_time,time.time())
